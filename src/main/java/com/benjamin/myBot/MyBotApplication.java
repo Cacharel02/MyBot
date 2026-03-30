13fcdc8067f92db0eb7e691649b88fee2e32f30d
Benjamin Nana <cachareljunior@gmail.com>
2026-03-30 21:30:53 +0200

    first commit

---
+++ src/main/java/com/benjamin/myBot/MyBotApplication.java
@@ -0,0 +1,13 @@
+package com.benjamin.myBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benjamin/myBot/MyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin.myBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
